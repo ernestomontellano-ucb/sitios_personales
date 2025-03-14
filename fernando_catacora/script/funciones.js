cd2ce64498efabbfa2ff687fd998cfee1833718a
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-03-14 19:11:32 -0400

    Header final añadido

--- fernando_catacora/script/funciones.js
+++ fernando_catacora/script/funciones.js
@@ -1,2 +1,19 @@
-let numero=45;
-Document.write("4");
+
+/*Menu responsive*/
+let boton=document.getElementById("open"),
+    botonClose=document.getElementById("close"),
+    menuR=document.getElementById("des"),
+    enlace=document.getElementById("enlaces");
+//click abrir
+boton.addEventListener("click", function () {
+    menuR.classList.add("active");
+})   
+//Click cerrar
+botonClose.addEventListener("click", function () {
+    menuR.classList.remove("active");
+})
+//Cerrar menu al hacer click en algun enlace
+enlace.addEventListener("click", function () {
+    menuR.style.transitionDelay = "0.5s";
+    menuR.classList.remove("active");
+})
