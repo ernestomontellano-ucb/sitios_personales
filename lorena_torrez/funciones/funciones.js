dc923e8b1390609983b30288b0309ca04e5a1917
Lorena Torrez <lorena.torrez@ucb.edu.bo>
2025-03-15 18:37:11 -0400

    casi pierdo todo :) header, footer, index y biografía hechos

---
+++ lorena_torrez/funciones/funciones.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const moon = document.getElementById("moon");
+    const phases = [
+      "new-moon",
+      "crescent-moon",
+      "half-moon",
+      "gibbous-moon ",
+      "full-moon",
+    ];
+    let currentPhase = 0;
+  
+    moon.addEventListener("click", () => {
+      moon.classList.remove(phases[currentPhase]);
+      currentPhase = (currentPhase + 1) % phases.length;
+      moon.classList.add(phases[currentPhase]);
+    });
+  
+    // Generate countless stars
+    const starsContainer = document.querySelector(".stars");
+    const numStars = 750; // Adjust this number for more or fewer stars
+  
+    for (let i = 0; i < numStars; i++) {
+      const star = document.createElement("div");
+      star.classList.add("star");
+      star.style.top = `${Math.random() * 100}vh`;
+      star.style.left = `${Math.random() * 100}vw`;
+      star.style.animationDuration = `${Math.random() * 5 + 2}s`; // Random duration between 2s and 7s
+      star.style.animationDelay = `${Math.random() * 5}s`; // Random delay between 0s and 5s
+      star.style.opacity = "0.2"; // Ajusta este valor según qué tan tenue quieres las estrellas
+      starsContainer.appendChild(star);
+    }
+  
+    // Generate moving stars
+  
+    const movingStarsContainer = document.querySelector(".moving-stars");
+    const numMovingStars = 20; // Adjust this number for more or fewer moving stars
+  
+    for (let i = 0; i < numMovingStars; i++) {
+      const movingStar = document.createElement("div");
+      movingStar.classList.add("moving-star");
+      movingStar.style.top = `${Math.random() * 100}vh`;
+      movingStar.style.left = `${Math.random() * 100}vw`;
+      movingStar.style.animationDuration = `${Math.random() * 10 + 10}s`; // Random duration between 5s
+      movingStarsContainer.appendChild(movingStar);
+    }
+  });
