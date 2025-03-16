cc2fb569d551f88b7910a5ea011946b61f5a1c9c
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-03-15 23:20:01 -0400

    Cuarta vercion ya actulizada

--- fernando_catacora/script/funciones.js
+++ fernando_catacora/script/funciones.js
@@ -17,3 +17,45 @@ enlace.addEventListener("click", function () {
     menuR.style.transitionDelay = "0.5s";
     menuR.classList.remove("active");
 })
+
+//Formulario
+let formulario=document.getElementById("formulario1");
+
+//eventos del formualrio
+    function validar(e) {
+        e.preventDefault();
+        let inputN=document.getElementById("nombre"),
+            inputT=document.getElementById("telefono"),
+            inputC=document.getElementById("correo"),
+            inputM=document.getElementById("mensaje"),
+            alertS=document.getElementById("a-s"),
+            alertF=document.getElementById("a-f");
+        if (inputN.value== "" || inputC.value=="" || inputT.value=="" || inputM.value==""){
+          
+            alertF.classList.remove("hide");
+            alertF.classList.add("show");
+            setTimeout(function () {
+                alertF.classList.remove("show");
+                alertF.classList.add("hide");
+
+            },2500)
+            
+        }else{
+        
+            alertS.classList.remove("hide");
+            alertS.classList.add("show");
+           
+            setTimeout(function () {
+                alertS.classList.remove("show");
+                alertS.classList.add("hide");
+
+            },2500)
+            inputC.value="";
+            inputM.value="";
+            inputN.value="";
+            inputT.value="";
+        }    
+    }
+
+
+formulario.addEventListener("submit", validar)
